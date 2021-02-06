556d07b80bb8c8057c6c8fd14a29deabf1c9b250
geoffroy <geoffroy.dutot@jymeo.net>
2021-02-06 13:50:25 +0100

    Infinite scroll

--- public/js/global.js
+++ public/js/global.js
@@ -12,6 +12,7 @@ function showMedias() {
         }
     });
 }
+
 // Load an alert message with animation
 function loadAlert() {
     document.getElementById('alert').classList.remove('opacity-0');
@@ -35,4 +36,69 @@ function hiddeAlert() {
 // Run removeAlert after some time
 function loadDisappearAlert() {
     setTimeout(removeAlert, 5000);
+}
+
+function infiniteScroll() {
+    var canLoadMoreTricks = true;
+
+    window.onscroll = function() {
+
+        // @var int totalPageHeight
+        var totalPageHeight = document.body.scrollHeight;
+
+        // @var int scrollPoint
+        var scrollPoint = window.scrollY + window.innerHeight;
+
+        // check if we hit the bottom of the page
+        if(scrollPoint >= totalPageHeight)
+        {
+            var groupTrick = document.getElementById('group-trick');
+            var countTricks = groupTrick.childElementCount;
+            var loadMoreTricksButton = document.getElementById('load-more-tricks');
+
+            function addMoreTricks() {
+                if (loadMoreTricksButton.classList.contains('hidden') && canLoadMoreTricks) {
+                    loadMoreTricksButton.classList.remove('hidden');
+                    fetch('/?offset='+countTricks, {
+                        method: 'GET',
+                        headers: {
+                            'X-Requested-With': 'XMLHttpRequest'
+                        }
+                    })
+                        .then(response => response.json())
+                        .then(response => {
+                            if (response.tricks.length < 8) {
+                                canLoadMoreTricks = false;
+                            }
+                            response.tricks.forEach(
+                                trick => {
+                                    var cardTrick =
+                                        "<div class='trick-card rounded-lg shadow-lg bg-white max-w-md overflow-hidden my-4'>" +
+                                            "<a href='/tricks/details/"+trick.slug+"'>" +
+                                                "<img src='/img/tricks/"+trick.picture+"' alt='' class='h-80 w-full'>" +
+                                            "</a>" +
+                                            "<div class='px-6 py-4 flex'>" +
+                                                "<h3 class='font-bold w-full'>"+"<a href='/tricks/details/"+trick.slug+"'>"+trick.title+"</a></h3>";
+                                    if (response.connected) {
+                                        cardTrick +=
+                                                "<div class=\"flex\">" +
+                                                    "<a href=\"\"><img src=\"/img/icons/edit-pen.svg.png\" alt=\"edit-icon\" class=\"object-contain h-7\"></a>" +
+                                                    "<a href=\"\" class=\"pl-4\"><img src=\"/img/icons/trash.svg.png\" alt=\"delete-icon\" class=\"object-contain h-7\"></a>" +
+                                                "</div>";
+                                    }
+                                    cardTrick +=
+                                            "</div>" +
+                                        "</div>";
+
+                                    groupTrick.innerHTML += cardTrick;
+                                });
+                            loadMoreTricksButton.classList.add('hidden');
+                        })
+                        .catch(error => console.log("Error : " + error));
+                }
+            }
+
+            setTimeout(addMoreTricks, 900);
+        }
+    }
 }
