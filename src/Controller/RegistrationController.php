fe3156486a1e708be8cb55390722b663514c0a89
geoffroy <geoffroy.dutot@jymeo.net>
2021-04-02 15:11:03 +0200

    User Register Validation by Email

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,12 +5,17 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Security\LoginFormAuthenticator;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 
 class RegistrationController extends AbstractController
@@ -19,8 +24,13 @@ class RegistrationController extends AbstractController
     /**
      * @Route("/register", name="app_register")
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator): Response
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator, TokenGeneratorInterface $tokenGenerator, MailerInterface $mailer): Response
     {
+        if ($this->getUser()) {
+            $this->addFlash('error', 'Already logged-in !');
+            return $this->redirectToRoute('home');
+        }
+
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
@@ -50,20 +60,61 @@ class RegistrationController extends AbstractController
                 $user->setProfilePicture($fileName);
             }
 
+            $user->setValidated(false);
+            $token = $tokenGenerator->generateToken();
+
+            try {
+                $user->setValidationToken($token);
+            } catch (\Exception $e) {
+                $this->addFlash('warning', $e->getMessage());
+                return $this->redirectToRoute('app_register');
+            }
+
+            $url = $this->generateUrl('validate_user', ['validationToken' => $user->getValidationToken()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+            $email = (new TemplatedEmail())
+                ->from(new Address('contact@snowtricks.fr', 'Snowtricks'))
+                ->to($user->getEmail())
+                ->subject('Validate you registration')
+                ->htmlTemplate('email/validate_registration.html.twig')
+                ->context([
+                    'user' => $user,
+                    'url' => $url,
+                    'emailContact' => 'contact@snowtricks.fr'
+                ]);
+
+            $mailer->send($email);
+
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
 
-            return $guardHandler->authenticateUserAndHandleSuccess(
-                $user,
-                $request,
-                $authenticator,
-                'main' // firewall name in security.yaml
-            );
+            $this->addFlash('success', 'Email validation sent. Please check you mails.');
+            $this->redirectToRoute('app_register');
         }
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/register/user/{validationToken}", name="validate_user")
+     */
+    public function validateUser(User $user, Request $request, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator)
+    {
+        $user->setValidationToken(null);
+        $user->setValidated(true);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        return $guardHandler->authenticateUserAndHandleSuccess(
+            $user,
+            $request,
+            $authenticator,
+            'main' // firewall name in security.yaml
+        );
     }
 }
